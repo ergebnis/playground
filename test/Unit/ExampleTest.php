f32f5c08168cadd4c9d360ccb861010f89b22871
Andreas Möller <am@localheinz.com>
2021-01-02 16:11:14 +0100

    Enhancement: Synchronize with ergebnis/php-library-template

--- test/Unit/ExampleTest.php
+++ test/Unit/ExampleTest.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) 2019-2021 Andreas Möller
+ * Copyright (c) 2017-2021 Andreas Möller
  *
  * For the full copyright and license information, please view
  * the LICENSE.md file that was distributed with this source code.
  *
- * @see https://github.com/ergebnis/playground
+ * @see https://github.com/ergebnis/php-library-template
  */
 
 namespace Ergebnis\Playground\Test\Unit;
